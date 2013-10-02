af1f618cd3cce4e61562fa58e0c4945aad53f7a6
Greg Borenstein <greg.borenstein@gmail.com>
2013-10-02 10:32:23 -0400

    basic text input. adjust planes proportionally to eliminate distortion

--- unity-game/Assets/LevelTimer.cs
+++ unity-game/Assets/LevelTimer.cs
@@ -6,11 +6,25 @@ public class LevelTimer : MonoBehaviour {
 	
 	//OVRGUI ovrGui;
 	//public OVRCameraController cameraController;
-
+	
+	string roomNumber = "";
 	
 	// Use this for initialization
-	void Start () {
+	void Start () {		
+		
+	}
 	
+	void OnGUI(){
+		Event e = Event.current;
+
+        if (e.keyCode == KeyCode.Return){
+			print("room number submitted: " + roomNumber);
+
+		} else {
+			GUI.SetNextControlName("RoomInput");
+			roomNumber = GUI.TextField(new Rect(300, 350, 200, 20), roomNumber, 25);
+			GUI.FocusControl("RoomInput");
+		}
 	}
 	
 	public float GetTimeRemaining(){
--- unity-game/Assets/WebsocketSwitch.cs
+++ unity-game/Assets/WebsocketSwitch.cs
@@ -22,7 +22,6 @@ public class WebsocketSwitch : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		print (levelTimer.GetTimeRemaining());
 		ws.Send(levelTimer.GetTimeRemaining().ToString("0.000"));
 		if(Input.GetKeyDown(KeyCode.Space)){
 			ws.Send("switch");
