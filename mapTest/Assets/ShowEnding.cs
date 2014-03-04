d5372076c93087f3f65b4bbb26b8f1fc61d268ad
Greg Borenstein <greg.borenstein@gmail.com>
2014-03-04 17:19:18 -0500

    update ending screen to have centered text

--- mapTest/Assets/ShowEnding.cs
+++ mapTest/Assets/ShowEnding.cs
@@ -16,7 +16,7 @@ public class ShowEnding : MonoBehaviour {
 				Color before = GUI.color;
 				GUI.color = new Color(0,1,0, Mathf.Sin(Time.time*15)*0.3f + 0.7f);
 
-				GUI.Label(new Rect(12,32, 100, 20), "Run time:\n" + finalTime.ToString("f2"), timerStyle);
+				GUI.Label(new Rect(200,0, 100, 20), "Run time:\n" + finalTime.ToString("f2"), timerStyle);
 
 				GUI.color = before;
 		GUI.DrawTexture(new Rect(0,0,512,512), endingImage, ScaleMode.ScaleToFit, true, 0.0f);
